be8518939b81ab9c02242ed9e7e8537ac5d2f175
rizkynandaanggia <127828416+rizkynandaanggia@users.noreply.github.com>
2023-04-10 23:07:48 +0700

    Add files via upload

---
+++ LatihanPert5Overloading/Overloading.cs
@@ -0,0 +1,32 @@
+﻿using LatihanPert5Overloading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPert5Overloading
+{
+    internal class MindanMax
+    {
+        public int MencariNilaiMin(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        public double MencariNilaiMin(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+        public int MencariNilaiMax(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public double MencariNilaiMax(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+
+    }
+}
---
+++ LatihanPert5Overloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using LatihanPert5Overloading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPert5Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MindanMax MindanMax1 = new MindanMax();
+
+            Console.WriteLine("=========================\nMencari Nilai Min dan Max\n=========================");
+            Console.WriteLine("Nilai a : 20.23\nNilai b : 10.04\n===================================================");
+            Console.WriteLine("Nilai Min dari kedua nilai tersebut adalah : {0}", MindanMax1.MencariNilaiMin(20.23, 10.04));
+            Console.WriteLine("Nilai Max dari kedua nilai tersebut adalah : {0}", MindanMax1.MencariNilaiMax(20.23, 10.04));
+            Console.ReadLine();
+        }
+    }
+}
